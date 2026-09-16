6cfc9ff56959969359526cf8aaaed9835596ec26
mohamedzarqawii <s23512686eg@std.aou.edu.eg>
2026-09-17 01:08:21 +0300

    fix: correct styling issues in Footer and AboutUsPage for improved layout consistency

--- features/aboutUs/AboutUsPage.tsx
+++ features/aboutUs/AboutUsPage.tsx
@@ -1,25 +1,27 @@
 const AboutUsPage = () => {
   return (
-    <div className="md:mx-10">
+    <div className="mx-4 flex h-[calc(100dvh-155px)] flex-wrap items-center justify-center gap-6 md:mx-10 md:gap-10 lg:h-[calc(100dvh-185px)]">
       {/* body */}
 
       <div className="flex flex-row md:flex-col">
         {/* first slide */}
-        <div className="mt-15 flex items-center justify-between">
+        <div className="flex items-center justify-between">
           {/* image */}
 
           <img
             src="/images/dummbles10.jpeg"
-            className="hidden w-50 rounded-xl object-cover object-left shadow-[0_0_15px] shadow-[#FEFEFE] md:block md:h-170 md:w-400 md:rounded-[65px]"
+            className="hidden max-h-160 w-full rounded-[65px] object-cover object-left shadow-[0_0_15px] shadow-[#FEFEFE] md:block"
           />
 
           {/* text */}
 
           <div className="flex flex-col gap-15 md:ml-20">
-            <div className="flex flex-col gap-10">
-              <div className="text-primary text-4xl">OUR MESSION</div>
-              <div className="flex flex-col gap-15">
-                <div className="text-lg">
+            <div className="flex flex-col gap-4 md:gap-10">
+              <div className="text-primary text-2xl md:text-4xl">
+                OUR MESSION
+              </div>
+              <div className="flex flex-col gap-7 md:gap-15">
+                <div className="text-sm md:text-lg">
                   Founded in 2024,
                   <span className="text-secondary"> ZEKA SPORTS </span>
                   started with a single goal: to provide high-quality equipment
@@ -28,17 +30,17 @@ const AboutUsPage = () => {
                   you.
                 </div>
 
-                <div className="flex gap-10">
-                  <div className="border-primary flex flex-col gap-2 border-l-2 p-2">
+                <div className="flex gap-2 md:gap-10">
+                  <div className="border-primary flex flex-col gap-2 border-l-2 pl-2">
                     <div className="text-primary">Durability</div>
-                    <div className="text-[12px]">
+                    <div className="text-xs">
                       Built to last the toughest training sessions.
                     </div>
                   </div>
 
-                  <div className="border-primary flex flex-col gap-2 border-l-2 p-2">
+                  <div className="border-primary flex flex-col gap-2 border-l-2 pl-2">
                     <div className="text-primary">Innovation</div>
-                    <div className="text-[12px]">
+                    <div className="text-xs">
                       Constantly refining materials and design.
                     </div>
                   </div>
