f17d125f6fe5efcf1586506bc3c05722d773f55c
mohamedzarqawii <s23512686eg@std.aou.edu.eg>
2026-09-18 07:45:11 +0300

    fix: update AboutUsPage to use Next.js Image component and improve text formatting; add mcp.json for console-ninja configuration

--- features/aboutUs/AboutUsPage.tsx
+++ features/aboutUs/AboutUsPage.tsx
@@ -1,20 +1,22 @@
+import Image from "next/image";
+
 const AboutUsPage = () => {
   return (
-    <div className="mx-4 flex h-[calc(100dvh-155px)] flex-wrap items-center justify-center gap-6 md:mx-10 md:gap-10 lg:h-[calc(100dvh-185px)]">
+    <div className="mx-4 flex h-[calc(100dvh-155px)] flex-wrap items-center justify-center gap-6 md:mx-10 md:gap-10 lg:max-h-[calc(100dvh-185px)]">
       {/* body */}
-
       <div className="flex flex-row md:flex-col">
         {/* first slide */}
         <div className="flex items-center justify-between">
           {/* image */}
-
-          <img
+          <Image
             src="/images/dummbles10.jpeg"
+            alt="Image"
+            width={400}
+            height={160}
             className="hidden max-h-160 w-full rounded-[65px] object-cover object-left shadow-[0_0_15px] shadow-[#FEFEFE] md:block"
           />
 
           {/* text */}
-
           <div className="flex flex-col gap-15 md:ml-20">
             <div className="flex flex-col gap-4 md:gap-10">
               <div className="text-primary text-2xl md:text-4xl">
@@ -26,8 +28,8 @@ const AboutUsPage = () => {
                   <span className="text-secondary"> ZEKA SPORTS </span>
                   started with a single goal: to provide high-quality equipment
                   to athletes who are passionate about their craft. We believe
-                  that the right gear shouldn't just fit you; it should inspire
-                  you.
+                  that the right gear shouldn&apos;t just fit you it should
+                  inspire you.
                 </div>
 
                 <div className="flex gap-2 md:gap-10">
