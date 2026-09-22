6a51b2b551601a5111734f06a2ffde2fd34965a2
New Account Name <mohamedzarqawi1@gmail.com>
2026-09-22 15:30:09 +0300

    refactor: reorganize EditCategoryPage layout; enhance form structure and improve product management display

--- features/dashboard/pages/ categories/EditCategoryPage.tsx
+++ features/dashboard/pages/ categories/EditCategoryPage.tsx
@@ -81,77 +81,92 @@ export const EditCategoryPage = ({ categoryId }: ViewProps) => {
   return (
     category && (
       <div>
-        <div className="flex items-center justify-between">
-          <div className="text-primary text-3xl">{category.name}</div>
-          <Button
-            variant={"outline"}
-            onClick={() => {
-              router.push(`/admin/categories/${category.id}`);
-            }}
-          >
-            View Mode
-          </Button>
-        </div>
-        {/* Basic information */}
-        <div className="border-primary mt-10 flex h-fit w-full flex-col rounded-3xl border bg-[#1a1a1a]/20 px-8 py-8 backdrop-blur-md">
-          <div className="text-lg">Basic Information</div>
-          <div className="mt-5 flex flex-wrap gap-6">
-            <Field>
-              <FieldLabel className="text-primary text-sm">ID</FieldLabel>
-              <div className="text-muted-foreground">{category?.id}</div>
-            </Field>
-
-            <Field>
-              <FieldLabel className="text-primary text-sm">
-                Created At
-              </FieldLabel>
-              <div className="text-muted-foreground">
-                {formattedDate} ({formattedTime})
-              </div>
-            </Field>
-
-            <Input
-              id="name"
-              name="name"
-              type="string"
-              label="Name"
-              isRequired={true}
-              errors={errors}
-              touched={touched}
-              value={values.name}
-              onChange={handleChange}
-              className="w-full sm:w-96"
-              aria-invalid={!!errors.name && !!touched.name}
-            />
+        <form onSubmit={handleSubmit}>
+          <div className="flex items-center justify-between">
+            <div className="text-primary text-3xl">{category.name}</div>
+            <Button
+              variant={"outline"}
+              onClick={() => {
+                router.push(`/admin/categories/${category.id}`);
+              }}
+            >
+              View Mode
+            </Button>
           </div>
-        </div>
+          {/* Basic information */}
+          <div className="border-primary mt-10 flex h-fit w-full flex-col rounded-3xl border bg-[#1a1a1a]/20 px-8 py-8 backdrop-blur-md">
+            <div className="text-lg">Basic Information</div>
+            <div className="mt-5 flex flex-wrap gap-6">
+              <Field>
+                <FieldLabel className="text-primary text-sm">ID</FieldLabel>
+                <div className="text-muted-foreground">{category?.id}</div>
+              </Field>
 
-        {/* Brand details */}
-        <div className="border-primary mt-6 flex h-fit w-full flex-col rounded-3xl border bg-[#1a1a1a]/20 px-8 py-8 backdrop-blur-md">
-          <div className="flex justify-between">
-            <div className="text-lg">Manage Products</div>
-            <AddProductToCategory categoryId={categoryId} />
+              <Field>
+                <FieldLabel className="text-primary text-sm">
+                  Created At
+                </FieldLabel>
+                <div className="text-muted-foreground">
+                  {formattedDate} ({formattedTime})
+                </div>
+              </Field>
+
+              <Input
+                id="name"
+                name="name"
+                type="string"
+                label="Name"
+                isRequired={true}
+                errors={errors}
+                touched={touched}
+                value={values.name}
+                onChange={handleChange}
+                className="w-full sm:w-96"
+                aria-invalid={!!errors.name && !!touched.name}
+              />
+            </div>
           </div>
-          <div className="mt-5 flex flex-wrap gap-6">
-            <Field>
-              <FieldLabel className="text-primary text-sm">
-                Related Products
-              </FieldLabel>
-              <div className="mt-2 grid w-full grid-cols-2 gap-3 md:grid-cols-[repeat(auto-fill,minmax(240px,1fr))] lg:gap-6">
-                {relatedProducts?.map((product, i) => (
-                  <ProductCardAdmin
-                    key={product.id}
-                    product={product}
-                    pageType="category"
-                    isAdmin={true}
-                    isLoading={isRelatedProductsLoading}
-                  />
-                ))}
-                {relatedProducts?.length == 0 && "-"}
-              </div>
-            </Field>
+
+          {/* Brand details */}
+          <div className="border-primary mt-6 flex h-fit w-full flex-col rounded-3xl border bg-[#1a1a1a]/20 px-8 py-8 backdrop-blur-md">
+            <div className="flex justify-between">
+              <div className="text-lg">Manage Products</div>
+              <AddProductToCategory categoryId={categoryId} />
+            </div>
+            <div className="mt-5 flex flex-wrap gap-6">
+              <Field>
+                <FieldLabel className="text-primary text-sm">
+                  Related Products
+                </FieldLabel>
+                <div className="mt-2 grid w-full grid-cols-2 gap-3 md:grid-cols-[repeat(auto-fill,minmax(240px,1fr))] lg:gap-6">
+                  {relatedProducts?.map((product, i) => (
+                    <ProductCardAdmin
+                      key={product.id}
+                      product={product}
+                      pageType="category"
+                      isAdmin={true}
+                      isLoading={isRelatedProductsLoading}
+                    />
+                  ))}
+                  {relatedProducts?.length == 0 && "-"}
+                </div>
+              </Field>
+            </div>
           </div>
-        </div>
+
+          <div className="mt-6 flex justify-end gap-3">
+            <Button
+              type="submit"
+              variant="default"
+              isPending={isCategoryLoading}
+              pendingText="Updating"
+              disabled={!dirty || isCategoryLoading}
+              className="rounded-lg p-6 text-base hover:cursor-pointer"
+            >
+              Update User
+            </Button>
+          </div>
+        </form>
       </div>
     )
   );
